1cf72e717c2bffb441df43daf60992d0e7f42d27
lukebrooke <lb3g20@soton.ac.uk>
2023-10-25 13:35:22 +0100

    added and documented delete function and in_test function

---
+++ R/delete.R
@@ -0,0 +1,27 @@
+#' Will delete a question or a test from the collection of all questions or tests
+#'
+#' @param x The name of the question or test that should be deleted
+#' @param removefrom The collection of either questions or tests that the items should be deleted
+#'
+#' @return The collection of questions or tests with the named item deleted
+#' @export
+
+delete <- function(x, removefrom){
+  UseMethod('delete')
+}
+
+delete.character <- function(x = character(), removefrom){
+  if(is.data.frame(removefrom)){
+    for(i in x){
+      removefrom <- removefrom[!(removefrom$name == i),]
+
+    }
+    removefrom <- removefrom
+  }
+  else{
+    for(i in x){
+      removefrom <- removefrom[!sapply(removefrom, '[[', 1) == i]
+    }
+    removefrom <- removefrom
+  }
+}
---
+++ R/in_test.R
@@ -0,0 +1,28 @@
+#' Will get a list of all tests that a question is contained in
+#'
+#' @param x The name of the question
+#' @param all_tests The list of all currently saved tests
+#'
+#' @return A list of the names of tests that the question is contained in
+#' @export
+
+in_test <-function(x = character(),all_tests = list()){
+  UseMethod('in_test')
+}
+
+in_test.default <-function(x = character(),all_tests = list()){
+  t <- c()
+  if(length(all_tests) == 0){
+    t<-t
+  }else{
+    for(j in x){
+      for(i in 1:length(all_tests)){
+        if (j %in% all_tests[[i]] && !(all_tests[[i]][1] %in% t))
+        {
+          t <- c(t, all_tests[[i]][1])
+        }
+      }
+    }
+    t<-t
+  }
+}
