e2496089e9d9b370e02827ee2d424ae1e5e0ada0
lukebrooke <lb3g20@soton.ac.uk>
2023-10-25 13:03:37 +0100

    Add and document the funtions to load questions and tests

---
+++ R/load_all.R
@@ -0,0 +1,53 @@
+#' Will load the names of all the tests currently stored in 'tests.csv'
+#'
+#' @return A list of the names of all the currently saved tests
+#' @export
+
+load_all_tests <-function(){
+  UseMethod('load_all_tests')
+}
+
+load_all_tests.default <- function()
+{
+  if(file.exists("tests.csv")){
+    temp <- read.csv(file = "tests.csv", header = FALSE)
+    all_ts <- list()
+    for(i in 1:length(temp[1,])){
+      name <- temp[1,i]
+      qs <- c()
+      for(j in 2:length(temp[,i])){
+        if(is.na(temp[j,i])){
+          break
+        }else{
+          qs<- c(qs,temp[j,i])
+        }
+        all_ts[[i]] <- c(name,qs)
+      }
+    }
+    all_ts <- all_ts
+  }
+  else{
+    all_ts <- NULL
+  }
+}
+
+#' Will load all the questions currently saved in 'questions.csv'
+#'
+#' @return A data.frame of all the currently saved questions
+#' @export
+
+load_all_questions <-function()
+{
+  UseMethod('load_all_questions')
+}
+
+load_all_questions.default <- function()
+{
+  if(file.exists("questions.csv"))
+  {
+    all_questions <- read.csv(file = "questions.csv", header = TRUE)
+  }else
+  {
+    all_questions <- NULL
+  }
+}
---
+++ R/save_item.R
@@ -0,0 +1,81 @@
+#' Will save the data.frame of questions or a list of tests to a .csv file
+#'
+#' @param x The item to be saved
+#'
+#' @return NA
+#' @export
+
+
+save_item <- function(x){
+  UseMethod("save_item")
+}
+
+
+#Made for testing/development, should not be used by the user in its current state
+save_item.question <-function(x = question()){
+  if(length(x) == 0)
+  {
+    file.remove("questions.csv")
+  }
+  else{
+    if(!file.exists("questions.csv"))
+    {
+      file.create("questions.csv")
+      header <- matrix(c('name', 'option_num', 'correct_score', 'wrong_loss', 'partial_score', 'given_no_correct', 'correct',0,0,0,0,0,0,0),nrow = 2, byrow = TRUE)
+      write.table(header,file = "questions.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
+    }
+    q<- x[-length(x)]
+    q$correct <- paste(x$correct, collapse = ",")
+    write.table(q, file = 'questions.csv',append = TRUE, sep = ",", col.names = FALSE, row.names = FALSE)
+
+  }
+}
+
+save_item.data.frame <- function(x){
+
+  if(length(x) ==0)
+  {
+    file.remove("questions.csv")
+  }
+  else{
+    if(!file.exists("questions.csv"))
+    {
+      file.create("questions.csv")
+      header <- matrix(c('name', 'option_num', 'correct_score', 'wrong_loss', 'partial_score', 'given_no_correct', 'correct',0,0,0,0,0,0,0),nrow = 2, byrow = TRUE)
+      write.table(header,file = "questions.csv", sep = ",", col.names = FALSE, row.names = FALSE, quote = FALSE)
+    }
+    write.table(x, file = 'questions.csv',append = FALSE, sep = ",", col.names = TRUE, row.names = FALSE)
+  }
+}
+
+save_item.list <- function(x){
+  if(length(x) == 0){
+    if(file.exists("tests.csv")){
+      file.remove("tests.csv")
+    }
+  }else{
+    if(!file.exists("tests.csv"))
+    {
+      file.create("tests.csv")
+    }
+    m<-0
+    for(i in 1:length(x))
+    {
+      if(length(x[[i]]) > m){m <-length(x[[i]])}
+    }
+    print(m)
+    for(i in 1:length(x))
+    {
+      extra <- m-length(x[[i]])
+      print(extra)
+      x[[i]] <- c(x[[i]], rep(NA,extra))
+    }
+    print(x)
+    write.table(x,file = 'tests.csv',append = FALSE, sep = ",", col.names = FALSE, row.names = FALSE)
+  }
+}
+
+save_item.default <- function(x)
+{
+  print("Tried to save null item")
+}
