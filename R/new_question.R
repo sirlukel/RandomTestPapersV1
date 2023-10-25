791a335cd1d7b56fa675856eed500d39f9a7789a
lukebrooke <lb3g20@soton.ac.uk>
2023-10-25 12:25:21 +0100

    Update new_student function

--- R/new_question.R
+++ R/new_question.R
@@ -11,10 +11,10 @@
 #'@export
 
 
-new_question <- function(option_num = integer(), correct_score = numeric(), wrong_loss = numeric(), partial_score = logical(), given_no_correct = logical(), correct = list()){
+new_question <- function(name = character(),option_num = integer(), correct_score = numeric(), wrong_loss = numeric(), partial_score = logical(), given_no_correct = logical(), correct = list()){
   #Updates score per correct answer if you are allowed partial marks
-  if (partial_score == TRUE) {correct_score <- correct_score/length(correct)}
-  structure(list(option_num = option_num,correct_score = correct_score, wrong_loss = wrong_loss, partial_score = partial_score, given_no_correct = given_no_correct,correct= sort(correct)), class = "question")
+  if (partial_score == TRUE) {correct_score <- as.numeric(correct_score)/length(correct)}
+  structure(list(name = name, option_num = option_num,correct_score = correct_score, wrong_loss = wrong_loss, partial_score = partial_score, given_no_correct = given_no_correct,correct= sort(correct)), class = "question")
 }
 
 
