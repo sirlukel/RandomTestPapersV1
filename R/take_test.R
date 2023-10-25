b9b521b0ea123754bec9da315ec2eac2f0f6d722
lukebrooke <lb3g20@soton.ac.uk>
2023-10-25 12:55:51 +0100

    added function and documentation for take_test

---
+++ R/take_test.R
@@ -0,0 +1,23 @@
+#' Makes a given student answer all the questions in a given test
+#'
+#' @param s The student that will take the test
+#' @param t The test that the student will answer
+#'
+#' @return A student with updated marks reflecting the results of answering the test
+#' @export
+
+take_test <- function(s = student(), t = test()){
+  UseMethod("take_test")
+}
+
+take_test.default<-function(s = student(), t = test()){
+  #Gets the given student to answer each question in a test in turn
+  for (i in t$questions)
+  {
+    s <- answer_question(s = s, q = i)
+  }
+  if (s$total_marks < 0 && t$can_neg == FALSE){
+    s$total_marks <- 0
+  }
+  s <- s
+}
